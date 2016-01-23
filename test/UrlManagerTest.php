683ec2726e787ef4cb146a9add472f07bc1b02e7
bassta <vasily.stashko@gmail.com>
2016-01-23 22:40:01 +0300

    + UrlManager::parseRequest() method test

--- test/UrlManagerTest.php
+++ test/UrlManagerTest.php
@@ -25,6 +25,36 @@ class UrlManagerTest extends PHPUnit_Framework_TestCase
 
     public function testParseRequest()
     {
+        $urlManager = new \UrlManager\UrlManager();
 
+        $urlManager->addRule(new \UrlManager\Rule('GET', '/users', 'user/list'))
+                   ->addRule(new \UrlManager\Rule('POST', '/users', 'user/list'))
+                   ->addRule(new \UrlManager\Rule('GET', '/user/{id:\d+}', 'user/view'))
+                   ->addRule(new \UrlManager\Rule('GET', '/man[/{id:\d+}]', 'man/view_op'));
+
+        $routeInfo = $urlManager->parseRequest('GET', '/not_found');
+        $this->assertEquals(FastRoute\Dispatcher::NOT_FOUND, $routeInfo[0]);
+
+        $routeInfo = $urlManager->parseRequest('PUT', '/users');
+        $this->assertEquals(FastRoute\Dispatcher::METHOD_NOT_ALLOWED, $routeInfo[0]);
+
+        $routeInfo = $urlManager->parseRequest('GET', '/users');
+        $this->assertEquals(FastRoute\Dispatcher::FOUND, $routeInfo[0]);
+        $this->assertEquals('user/list', $routeInfo[1]);
+
+        $routeInfo = $urlManager->parseRequest('GET', '/user/1');
+        $this->assertEquals(FastRoute\Dispatcher::FOUND, $routeInfo[0]);
+        $this->assertEquals('user/view', $routeInfo[1]);
+        $this->assertEquals([ 'id' => 1 ], $routeInfo[2]);
+
+        $routeInfo = $urlManager->parseRequest('GET', '/man/1');
+        $this->assertEquals(FastRoute\Dispatcher::FOUND, $routeInfo[0]);
+        $this->assertEquals('man/view_op', $routeInfo[1]);
+        $this->assertEquals([ 'id' => 1 ], $routeInfo[2]);
+
+        $routeInfo = $urlManager->parseRequest('GET', '/man');
+        $this->assertEquals(FastRoute\Dispatcher::FOUND, $routeInfo[0]);
+        $this->assertEquals('man/view_op', $routeInfo[1]);
+        $this->assertEquals([], $routeInfo[2]);
     }
 }
